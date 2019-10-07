918bbd6dcad9580d18532849ee182b731077c65a
sad1503 <vuductran153@gmail.com>
2019-10-07 20:54:16 +0900

    Ex2 Q2

--- Ex2/Q2/table.cpp
+++ Ex2/Q2/table.cpp
@@ -1,17 +1,31 @@
 #include <fstream>
 #include <iostream>
+#include <iomanip>
 
 
-ifstream fin("data.txt");
-if (!fin.is_open()) {
-	std::cerr << "Can not open file" << std::endl;
-	exit(1); }
-
-int number;
-int fnumber;
-
-while(!fin.eof()) {
+int main(void) {
 	
-}
+	std::ifstream file("data.txt");
+	if(!file.is_open()) {
+		std::cerr << "Can not open file" << std::endl;
+		exit(1); 
+	}
 
-fin.close();
+	int number;
+	double dnumber;
+
+	std::cout << "---------------------+---------------------+--------------------" << std::endl;
+
+	int i = 1;
+
+	while(!file.eof()) {
+		file >> number >> dnumber;
+		if(file.eof()) break;
+		std::cout << std::setw(20) << i << " |" << std::setw(20) << number << " |" << std::setw(20) << dnumber << std::endl;
+		i ++;
+	}
+
+	file.close();
+
+	return 0;
+}
