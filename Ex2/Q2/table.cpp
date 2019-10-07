86f9e065057d58237b035ff35b478fc3cbf5c406
s1252012 <s1252012@std5dc7.u-aizu.ac.jp>
2019-10-07 11:47:58 +0900

    Ex2 Q1

--- Ex2/Q2/table.cpp
+++ Ex2/Q2/table.cpp
@@ -2,10 +2,16 @@
 #include <iostream>
 
 
-ifstream input("data.txt");
-if (!input.is_open()) {
+ifstream fin("data.txt");
+if (!fin.is_open()) {
 	std::cerr << "Can not open file" << std::endl;
 	exit(1); }
+
 int number;
-input >> number;
-input.close();
+int fnumber;
+
+while(!fin.eof()) {
+	
+}
+
+fin.close();
