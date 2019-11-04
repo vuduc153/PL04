0c10baf148c997000eacf07b3b1cab26a7449820
sad1503 <vuductran153@gmail.com>
2019-11-04 15:56:52 +0900

    Ex8

--- Ex8/Q1/Mat3x3.cpp
+++ Ex8/Q1/Mat3x3.cpp
@@ -1,16 +1,14 @@
 #include "Mat3x3.h"
 
-// TODO
-
 Mat3x3::Mat3x3() {
-	for(int i = 0; i < 3; i ++) {
+	for(int i = 0; i < MATRIX_SIZE; i ++) {
 		matrix[i][i] = 1;
 	}
 }
 
-Mat3x3::Mat3x3(double **matrix) {
-	for(int i; i < MATRIX_SIZE; i ++) {
-		for(int j; j < 3; j ++) {
+Mat3x3::Mat3x3(double matrix[MATRIX_SIZE][MATRIX_SIZE]) {
+	for(int i = 0; i < MATRIX_SIZE; i ++) {
+		for(int j = 0; j < MATRIX_SIZE; j ++) {
 			this->matrix[i][j] = matrix[i][j];
 		}
 	}
@@ -23,36 +21,6 @@ Mat3x3::Mat3x3(const Mat3x3& m) {
 			this->matrix[i][j] = m.matrix[i][j];
 		}
 	} 
-}
-
-Mat3x3 Mat3x3::operator+ (const Mat3x3& m1, const Mat3x3& m2) {
-	double matrix[3][3];
-	for(int i = 0; i < MATRIX_SIZE; i ++) {
-		for(int j = 0; j < MATRIX_SIZE; j ++) {
-			matrix[i][j] = m1.matrix[i][j] + m2.matrix[i][j];
-		}
-	}
-	return Mat3x3(matrix);
-}
-
-Mat3x3 Mat3x3::operator- (const Mat3x3& m1, const Mat3x3& m2) {
-	double matrix[3][3];
-	for(int i = 0; i < MATRIX_SIZE; i ++) {
-		for(int j = 0; j < MATRIX_SIZE; j ++) {
-			matrix[i][j] = m1.matrix[i][j] - m2.matrix[i][j];
-		}
-	}
-	return Mat3x3(matrix);
-}
-
-Mat3x3 Mat3x3::operator* (const Mat3x3& m1, const Mat3x3& m2){
-	double matrix[3][3];
-	for(int i = 0; i < MATRIX_SIZE; i ++) {
-		for(int j = 0; j < MATRIX_SIZE; j ++) {
-			matrix[i][j] = m1.matrix[i][0] * m2.matrix[0][j] + m1.matrix[i][1] * m2.matrix[1][j] + m1.matrix[i][2] * m2.matrix[2][j]; 
-		}
-	}
-	return Mat3x3(matrix);
 }
 
 Mat3x3& Mat3x3::operator+= (const Mat3x3& m) {
@@ -82,6 +50,73 @@ Mat3x3& Mat3x3::operator*= (const Mat3x3& m) {
 	return *this;
 }
 
-Mat3x3& Mat3x3::operator- (const Mat3x3& m) {
+double& Mat3x3::operator()(int i, int j) {
+	return matrix[i][j];
+}
 
+const double& Mat3x3::operator()(int i, int j) const {
+ 	return matrix[i][j];	
+}
+
+Mat3x3 operator+ (const Mat3x3& m1, const Mat3x3& m2) {
+	double matrix[3][3];
+	for(int i = 0; i < MATRIX_SIZE; i ++) {
+		for(int j = 0; j < MATRIX_SIZE; j ++) {
+			matrix[i][j] = m1(i, j) + m2(i, j);
+		}
+	}
+	return Mat3x3(matrix);
+}
+
+Mat3x3 operator- (const Mat3x3& m1, const Mat3x3& m2) {
+	double matrix[3][3];
+	for(int i = 0; i < MATRIX_SIZE; i ++) {
+		for(int j = 0; j < MATRIX_SIZE; j ++) {
+			matrix[i][j] = m1(i, j) - m2(i, j);
+		}
+	}
+	return Mat3x3(matrix);
+}
+
+Mat3x3 operator* (const Mat3x3& m1, const Mat3x3& m2){
+	double matrix[3][3];
+	for(int i = 0; i < MATRIX_SIZE; i ++) {
+		for(int j = 0; j < MATRIX_SIZE; j ++) {
+			matrix[i][j] = m1(i, 0) * m2(0, j) + m1(i, 1) * m2(1, j) + m1(i, 2) * m2(2, j); 
+		}
+	}
+	return Mat3x3(matrix);
+}
+
+bool operator== (const Mat3x3& m1, const Mat3x3& m2) {
+	for(int i = 0; i < MATRIX_SIZE; i ++) {
+		for(int j = 0; j < MATRIX_SIZE; j ++) {
+			if(m1(i, j) != m2(i, j))
+				return false;
+		}
+	}
+	return true;
+}
+
+Mat3x3 operator- (const Mat3x3& m) {
+	double matrix[3][3];
+	for (int i = 0; i < MATRIX_SIZE; i ++) {
+		for (int j = 0; j < MATRIX_SIZE; j ++) {
+			matrix[i][j] = - m(i, j);
+		}
+	}
+	return Mat3x3(matrix);
+}
+
+std::ostream& operator<< (std::ostream& os, const Mat3x3 &m) {
+	std::ostringstream s;
+	for(int i = 0; i < MATRIX_SIZE; i ++) {
+		s << '[';
+		for(int j = 0; j < MATRIX_SIZE; j ++) {
+			s << ' ' << m(i, j) << ' ';
+		}
+		s << ']' << std::endl;;
+	} 
+	s << std::endl;
+	return os << s.str();
 }
--- Ex8/Q1/Mat3x3.h
+++ Ex8/Q1/Mat3x3.h
@@ -2,23 +2,19 @@
 #define MAT3x3_H
 #define MATRIX_SIZE 3
 #include <iostream>
+#include <istream>
+#include <ostream>
+#include <sstream>
 
 class Mat3x3 {
 private:
-	double matrix[3][3]
+	double matrix[MATRIX_SIZE][MATRIX_SIZE];
 public:
 	Mat3x3();
 
-	Mat3x3(double **matrix);
-
-	// Copy Constructor
+	Mat3x3(double matrix[MATRIX_SIZE][MATRIX_SIZE]);
+    // Copy Constructor
 	Mat3x3(const Mat3x3& m);
-
-    Mat3x3 operator+ (const Mat3x3& m1, const Mat3x3& m2);
-
-    Mat3x3 operator- (const Mat3x3& m1, const Mat3x3& m2);
-
-    Mat3x3 operator* (const Mat3x3& m1, const Mat3x3& m2);
 
     Mat3x3& operator+= (const Mat3x3& m);
 
@@ -26,15 +22,22 @@ public:
 
     Mat3x3& operator*= (const Mat3x3& m);
 
-    Mat3x3& operator- (const Mat3x3& m);
-
     double& operator()(int i, int j);
 
  	const double& operator()(int i, int j) const;
 
- 	bool operator== (const& Mat3x3 m1, const& Mat3x3 m2);
-
- 	std::ostream& operator<< (std::ostream& os, const Mat3x3 &c);    
 };
+
+Mat3x3 operator+ (const Mat3x3& m1, const Mat3x3& m2);
+
+Mat3x3 operator- (const Mat3x3& m1, const Mat3x3& m2);
+
+Mat3x3 operator* (const Mat3x3& m1, const Mat3x3& m2);
+
+bool operator== (const Mat3x3& m1, const Mat3x3& m2);
+
+Mat3x3 operator- (const Mat3x3& m);
+
+std::ostream& operator<< (std::ostream& os, const Mat3x3 &m);
 
 #endif
